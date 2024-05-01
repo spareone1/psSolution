cabd7f4b8837f57de19083cf5bbcaa9a2de88b6e
spareone <46594971+spareone1@users.noreply.github.com>
2024-05-01 22:20:00 +0900

    [Bronze V] Title: X보다 작은 수, Time: 48 ms, Memory: 32276 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    n, x = map(int, input().split())
+    arr = list(map(int, input().split()))
+    
+    for i in arr:
+        if i < x:
+            print(i, end = " ")
