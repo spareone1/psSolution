845e6d8425439a122b9c780dbdfc09f34b241430
spareone <46594971+spareone1@users.noreply.github.com>
2024-10-28 21:18:28 +0900

    [Bronze IV] Title: 나이 계산하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16199. 나이 계산하기/나이 계산하기.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int sol(int (*date)[3]) {
+    // 생년과 기준 연도가 동일하면 0살
+    if(date[1][0] == date[0][0]) return 0;
+    // 생일 전
+    if(date[1][1] < date[0][1] || date[1][1] == date[0][1] && date[1][2] < date[0][2]) return date[1][0] - date[0][0] - 1;
+    return date[1][0] - date[0][0];
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int date[2][3] = {0, };
+
+    for(int i = 0; i < 2; i++)
+        for(int j = 0; j < 3; j++)
+            cin >> date[i][j];
+
+    cout << sol(date) << '\n' << date[1][0] - date[0][0] + 1 << '\n' << date[1][0] - date[0][0] << '\n';
+}
