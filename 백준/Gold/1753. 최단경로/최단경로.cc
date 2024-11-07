01fca8a4176c97bc5cb8c175b4d7ac97b9a7e6ba
spareone <46594971+spareone1@users.noreply.github.com>
2024-11-07 20:43:05 +0900

    [Gold IV] Title: 최단경로, Time: 88 ms, Memory: 9192 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -2,12 +2,6 @@
 #define INF 987654321
 using namespace std;
 
-struct edge{
-    int node, dist;
-    edge(int node, int dist) : node(node), dist(dist) {}
-    bool operator<(const edge &e) const { return this->dist > e.dist; }
-};
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -16,18 +10,17 @@ int main() {
     cin >> v >> e >> k;
     vector<vector<pair<int, int>>> graph(v);
     vector<int> ans(v, INF);
-    priority_queue<edge> pq;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 
     for(int i = 0; i < e; i++) { int a, b, dist; cin >> a >> b >> dist; graph[a - 1].push_back({b - 1, dist}); }
 
-    pq.push(edge(k - 1, 0));
-    ans[k - 1] = 0;
+    pq.push({0, k - 1}); ans[k - 1] = 0;
     while(!pq.empty()) {
-        edge p = pq.top(); pq.pop();
-        if(ans[p.node] < p.dist) continue;
-        for(int i = 0; i < graph[p.node].size(); i++){
-            int next = graph[p.node][i].first, nextdist = graph[p.node][i].second + ans[p.node];
-            if(nextdist < ans[next]) { ans[next] = nextdist; pq.push({next, nextdist}); }
+        pair<int, int> p = pq.top(); pq.pop();
+        if(ans[p.second] < p.first) continue;
+        for(int i = 0; i < graph[p.second].size(); i++){
+            int next = graph[p.second][i].first, nextdist = graph[p.second][i].second + ans[p.second];
+            if(nextdist < ans[next]) { ans[next] = nextdist; pq.push({nextdist, next}); }
         }
     }
 
